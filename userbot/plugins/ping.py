c09b9ac87aeb93688bf919e27cba694895076e07
UltroidVenom <80195199+UltroidVenom@users.noreply.github.com>
2021-04-27 16:44:30 +0530

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -64,7 +64,7 @@ async def _(event):
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     await event.edit(
-        f"__**꧁ Pong! ꧂__**\n\n   ⚘ {ms}\n   ⚘ __**My**__ __**Master**__ [{DEFAULTUSER}]\n\n\n "
+        f"__**☞︎︎︎Pong!☜︎︎︎__**\n\n   ⚘ {ms}\n   𓇽 __**My**__ __**Master**__ [{DEFAULTUSER}]\n\n\n "
     )
 
 
